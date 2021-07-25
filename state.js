9e759d36c07b17668dce51630ace5817e043ac52
Nanguy <marchenrynanguy1@gmail.com>
2021-07-25 15:49:05 +0000

    ok

--- state.js
+++ state.js
@@ -4,15 +4,15 @@ const { createPath } = require("./common");
 exports.mapWritableState = (options) => {
   const object = {};
 
-  const base = options.prop ? this[options.prop] : options.base 
-  const path = createPath(base, true);
-
   options.fields.forEach((field) => {
     object[field] = {
       get() {
+        const base = options.prop ? this[options.prop] : options.base;
+        const path = base ?  createPath(base, true) : "";
         return _.get(this.$store.state, path + field);
       },
       set(value) {
+        const base = options.prop ? this[options.prop] : options.base;
         this.$store.commit(base + "/" + field, value);
       },
     };
